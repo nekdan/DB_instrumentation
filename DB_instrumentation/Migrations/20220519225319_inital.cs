4e812dda14fb9580061bd3aa2e26a5c09d4453cc
nekdan <nekdan@ya.ru>
2022-05-20 01:55:42 +0300

    добавил миграцию

---
+++ DB_instrumentation/Migrations/20220519225319_inital.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DB_instrumentation.Migrations
+{
+    public partial class inital : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    IconBase64 = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Instuments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instuments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NoteImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ImageBase64 = table.Column<string>(type: "TEXT", nullable: false),
+                    SoundId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SubsoundId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoteImages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sounds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    SubinstumentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    InstumentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SubinstrumentId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sounds", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SoundsDatas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    SoundBase64 = table.Column<string>(type: "TEXT", nullable: false),
+                    SoundId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SubsoundId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SoundsDatas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subinstuments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    InstumentId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subinstuments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subsounds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    SoundId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subsounds", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Instuments");
+
+            migrationBuilder.DropTable(
+                name: "NoteImages");
+
+            migrationBuilder.DropTable(
+                name: "Sounds");
+
+            migrationBuilder.DropTable(
+                name: "SoundsDatas");
+
+            migrationBuilder.DropTable(
+                name: "Subinstuments");
+
+            migrationBuilder.DropTable(
+                name: "Subsounds");
+        }
+    }
+}
